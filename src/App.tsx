679cda543bf0490fb091f89b0bb7404f494b9f70
Eliiia <eliiia@protonmail.com>
2026-07-21 20:15:55 +0100

    Add contact page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import Layout from './components/Layout'
 import Home from './pages/Home'
+import Contact from './pages/Contact'
 
 function App() {
   const path = window.location.pathname
 
-  return <Layout>{path === '/' && <Home />}</Layout>
+  return (
+    <Layout>
+      {path === '/' && <Home />}
+      {path === '/contact' && <Contact />}
+    </Layout>
+  )
 }
 
 export default App
---
+++ src/pages/Contact.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+import Button from '../components/Button'
+import Input from '../components/Input'
+import Title from '../components/Title'
+
+export default function Contact() {
+  const webhookUrl =
+    'https://discord.com/api/webhooks/1529196312547819611/W62DsO8_HGc4yyG80U5gTE_kvmkPYwVoTdm_pTKsK9EZGwehbQFiUCjrHyg1LiRpe34a'
+  const [user, setUser] = useState('')
+  const [content, setContent] = useState('')
+
+  const sendWebhook = () => {
+    fetch(webhookUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        username: user,
+        content: `${content}\n\n<@257482333278437377>`,
+      }),
+    }).then((res) => {
+      console.log('success')
+    })
+  }
+
+  return (
+    <>
+      <Title>Contact</Title>
+      <p></p>
+      <Input
+        type="text"
+        placeholder="Your name"
+        onChangeValue={(v) => setUser(v)}
+      />
+      <Input
+        type="textbox"
+        placeholder="Your message"
+        wide
+        onChangeValue={(v) => setContent(v)}
+      />
+      <br />
+      <Button variant="blue" onClick={sendWebhook}>
+        Send
+      </Button>
+    </>
+  )
+}
